e57d5c7c4e9acd3b9b43e71d39008c9f19a5b9fd
Katy Farmer <dreadpiratekaty@gmail.com>
2019-07-31 10:49:09 -0700

    Gatsby tutorial - pages

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+  <div style={{ color: `gray` }}>
+    <Header headerText="About Gatsby"/>
+    <Header headerText="Double Header"/>
+    <p>Such wow. Very React.</p>
+  </div>
+)
